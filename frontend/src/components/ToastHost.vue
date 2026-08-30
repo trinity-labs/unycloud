408586f4627ad31e69beeda6dba174e82c4b98c0
UnyCloud Maintainers <maintainers@trinity-labs.local>
2026-08-30 10:32:47 +0000

    unycloud: fix v0.6.15 2026-08-30T10:32:47Z
    Automated UnyCloud git sync.
    
    Path: /home/coder/TRINITY-DOCKER/var/docker/unycloud
    Timestamp UTC: 2026-08-30T10:32:47Z
    Changed file(s): 7
    Summary: 0A 7M 0D 0R
    
    Stat:
     README.md                             | 4 ++--
     RELEASE_NOTES.md                      | 4 ++--
     UNYCLOUD_VERSION                      | 2 +-
     docs/MAINTENANCE.md                   | 2 +-
     docs/installation.md                  | 2 +-
     frontend/package.json                 | 2 +-
     frontend/src/components/ToastHost.vue | 2 +-
     7 files changed, 9 insertions(+), 9 deletions(-)
    
    Files (first 80):
    M README.md
    M RELEASE_NOTES.md
    M UNYCLOUD_VERSION
    M docs/MAINTENANCE.md
    M docs/installation.md
    M frontend/package.json
    M frontend/src/components/ToastHost.vue

--- frontend/src/components/ToastHost.vue
+++ frontend/src/components/ToastHost.vue
@@ -39,7 +39,7 @@ import { removeToast, toasts } from "@/utils/toast";
   left: 50%;
   top: 1em;
   z-index: 10000;
-  width: min(32em, calc(100vw - 2em));
+  width: min(24rem, calc(100vw - 2rem));
   transform: translateX(-50%);
   display: flex;
   flex-direction: column;
